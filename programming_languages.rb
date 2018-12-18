5d0797be66435f1aa7658ef09ba8cc47856cb86c
Joseph O'Dell <josephodell@outlook.com>
2018-12-18 13:55:18 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,11 +3,9 @@ require 'pry'
 def reformat_languages(languages)
   new_hash= Hash.new
 
-  languages.each do |style, data|
-    data.each do |language, value|
-      new_hash[style] = value
+  ookeys = languages[:oo].keys
+  functionalkeys = languages[:functional].keys
 
-    end
-  end
-     new_hash
+  
+new_hash
 end
