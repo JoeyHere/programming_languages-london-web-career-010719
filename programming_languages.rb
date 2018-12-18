74437c5a1d3de9392a74ffba5ec283444159761e
Joseph O'Dell <josephodell@outlook.com>
2018-12-18 14:18:26 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,10 +4,8 @@ def reformat_languages(languages)
 new_hash = {}
 languages.each do |key, value|
   value.each do |name, info|
-    if new_hash[name] == nil
-      new_hash[name] = info
-      new_hash[name][:style] = []
-    end
+    new_hash[name] = info
+      new_hash[name][:style] = [] if new_hash[name] == nil
     new_hash[name][:style] << key
 end
 end
