420ae4788358aa58452e1db6dc6ed09ebc361106
Joseph O'Dell <josephodell@outlook.com>
2018-12-18 14:00:07 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,10 +2,16 @@ require 'pry'
 
 def reformat_languages(languages)
   new_hash= Hash.new
+  lang = languages[:oo].merge(languages[:functional])
 
   ookeys = languages[:oo].keys
   functionalkeys = languages[:functional].keys
 
-  
+lang.each do |key, values|
+  new_hash[key] = values
+
 new_hash
 end
+
+
+  game_hash[:home][:players].merge(game_hash[:away][:players])
