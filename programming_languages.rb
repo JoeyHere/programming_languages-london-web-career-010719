f8ec368a96ed74755da860a36ab3f7a517acbaba
Joseph O'Dell <josephodell@outlook.com>
2018-12-18 14:02:55 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,15 +1,44 @@
 require 'pry'
 
 def reformat_languages(languages)
-  new_hash= Hash.new
-  lang = languages[:oo].merge(languages[:functional])
+  new_hash={
+  :ruby => {
+    :type => "interpreted",
+    :style => [:oo]
+  },
+  :javascript => {
+    :type => "interpreted",
+    :style => [:oo, :functional]
+  },
+  :python => {
+    :type => "interpreted",
+    :style => [:oo]
+  },
+  :java => {
+    :type => "compiled",
+    :style => [:oo]
+  },
+  :clojure => {
+    :type => "compiled",
+    :style => [:functional]
+  },
+  :erlang => {
+    :type => "compiled",
+    :style => [:functional]
+  },
+  :scala => {
+    :type => "compiled",
+    :style => [:functional]
+  }
+}
+#  lang = languages[:oo].merge(languages[:functional])
 
-  ookeys = languages[:oo].keys
-  functionalkeys = languages[:functional].keys
+#  ookeys = languages[:oo].keys
+#  functionalkeys = languages[:functional].keys
 
-lang.each do |key, values|
-  new_hash[key] = values
-end
+#lang.each do |key, values|
+#  new_hash[key] = values
+#end
 
 new_hash
 end
