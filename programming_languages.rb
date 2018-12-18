e593396206ca413f5a0831574eaa59185aa682f6
Joseph O'Dell <josephodell@outlook.com>
2018-12-18 13:48:38 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,13 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  new_hash= Hash.new
+
+  languages.each do |style, data|
+    data.each do |language, values|
+      new_hash[language] = values
+
+
+  end
+    puts new_hash
 end
